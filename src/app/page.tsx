b30e5a0ba4aef805a4b5c7d6cc5cfc1f72427fb8
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-04 19:49:37 +0530

    the card hover completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import UpcommingWebnars from "@/components/UpcommingWebnars"
+
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -10,6 +12,8 @@ export default function Home() {
    <FeaturedCourses />
    <WhyChooseUs />
    <TestimonialCards />
+   <UpcommingWebnars/>
+
   </main>
   )
 }
---
+++ src/components/UpcommingWebnars.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import { Hovercard } from './Hovercard'
+
+const UpcommingWebnars = () => {
+    return (
+        <div
+            className='p-12 '
+        >
+            <div
+                className='max-w-7xl mx-auto px-4 sm:px-6'>
+                <div className='text-center'>
+                    <h2 className='text-base text-teal-600 font-semibold tracking-wide uppercase'>FEATURED WEBNARS</h2>
+                    <p className='mt-2 text-5xl leading-8
+                font-extrabold tracking-tight text-white'>Enhance Your Musical Journey</p>
+                </div>
+                <div className='mt-10'><Hovercard/></div>
+                <div className='mt-10 text-center'>
+                    <Link href="/courses" className="inline-block py-3 px-6 bg-teal-600 text-white rounded-lg shadow-md hover:bg-teal-700 transition-colors">
+                        View All Courses
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UpcommingWebnars
