0580158590c6767f9047fde0fbe7e63dad183c13
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-05 11:09:39 +0530

    flip the words under the sparkle is implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import UpcommingWebnars from "@/components/UpcommingWebnars"
 import Instructors from "@/components/Instructors";
+import SparklesUnder from "@/components/SparklesUnder";
 
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
    {/* <h1 className="text-3xl text-center">Ikhlas and Next.js</h1> */}
+   <SparklesUnder/>
    <HeroSection />
    <FeaturedCourses />
    <WhyChooseUs />
---
+++ src/components/SparklesUnder.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { SparklesCore } from "./ui/sparkles";
+import { FlipWords } from "./ui/flip-words";
+
+const SparklesUnder = () => {
+    const words = [" Music", "Art", "Happiness", "Peace"];
+    return (
+        <div className="h-[30rem] mt-16 w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1
+                className="md:text-7xl lg:text-6xl mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            > Welcome the World of <FlipWords words={words} /></h1>
+            <div className="w-[40rem] h-40 relative">
+                {/* Gradients */}
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+                {/* Core component */}
+                <SparklesCore
+                    background="transparent"
+                    minSize={0.4}
+                    maxSize={1}
+                    particleDensity={1200}
+                    className="w-full h-full"
+                    particleColor="#FFFFFF"
+                />
+
+                {/* Radial Gradient to prevent sharp edges */}
+                <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SparklesUnder
